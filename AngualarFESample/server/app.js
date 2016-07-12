008f37882d48338ccbfaada68ba0a9bc9df7904a
Dhanabalan <dhanabalan.java@gmail.com>
2016-07-12 17:40:09 +0530

    integrating openshift db

--- AngualarFESample/server/app.js
+++ AngualarFESample/server/app.js
@@ -8,13 +8,13 @@ var express=require('express');
 var app=express();
 var bodyparser=require("body-parser");
 var mongojs=require("mongojs");
-var db=mongojs("eventmanagement",['eventmanagement']);
+var db=mongojs("saradha",['saradha']);
 
 console.log(path.join(__dirname, '../')+"client/index_nodejs.html");
 app.use(express.static(path.join(__dirname, '../')+"client"));
 app.use(bodyparser.json());
 app.get('/volunteerlist',function(req,res){
-    db.eventmanagement.find().sort({_id:-1}).toArray(function(err,docs){
+    db.student.find().sort({_id:-1}).toArray(function(err,docs){
         console.log("Docs ::: "+JSON.stringify(docs));
         console.log("Docs ::: "+JSON.stringify(Object.keys(docs).length));
         res.json(docs);
@@ -24,7 +24,7 @@ app.get('/volunteerlist',function(req,res){
 app.post('/addvolunteer',function(req,res){
     console.log("from add");
     console.log(JSON.stringify(req.body));
-   db.eventmanagement.insert(req.body,function(err,docs){
+   db.student.insert(req.body,function(err,docs){
         res.json(docs);
         console.log("sho ::: "+JSON.stringify(docs));
     });
